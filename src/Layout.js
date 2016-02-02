1b065b037448bc03920645e7da5ae9f5ea263703
cybice <istarkov@gmail.com>
2016-02-02 21:23:47 +0300

    Fix layout texts

--- src/Layout.js
+++ src/Layout.js
@@ -10,16 +10,30 @@ export class Layout extends Component {
     return (
       <div className={layout}>
         <header className={header}>
-          <div>Clustering example google-map-react (zoom, move to play with)</div>
-          <div><a href="github.com">github.com</a></div>
+          <div>
+            Clustering example google-map-react (zoom, move to play with)
+          </div>
+          <div>
+            <a href="https://github.com/istarkov/google-map-clustering-example">
+              Star at github.com
+            </a>
+          </div>
         </header>
         <main className={main}>
           <GMap />
         </main>
         <footer className={footer}>
-          <div>Ivan Starkov</div>
+          <div>
+            <a href="https://github.com/istarkov">
+              Ivan Starkov
+            </a>
+          </div>
           <div className={logo}></div>
-          <div>@icelabaratory</div>
+          <div>
+            <a href="https://twitter.com/icelabaratory">
+              @icelabaratory
+            </a>
+          </div>
         </footer>
       </div>
     );
